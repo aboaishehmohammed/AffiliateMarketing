ef455c62e25e97bb3a4ab1e12def43c7a54c9e01
mohammed_aboaisheh <aboaishehmohammed@gmail.com>
2022-07-10 15:35:20 +0300

    affiliate

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('auth/login-api' , [\App\Http\Controllers\AuthController::class , 'loginApi']);
 
 Route::group(['middleware' => 'auth:sanctum'] , function (){
     Route::get('user/chart-data' , [\App\Http\Controllers\UserController::class , 'chartsData'])->name('user.chartData');
